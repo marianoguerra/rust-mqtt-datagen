a88c632c2dae8cd5e659e80c2173b70ded4e5484
Mariano Guerra <mariano@marianoguerra.org>
2019-12-16 17:16:32 +0100

    add kafka as sink

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,10 @@ use std::io::{self, Read};
 use std::thread;
 use std::time::Duration;
 
+use rdkafka;
+use rdkafka::config::ClientConfig;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+
 pub trait Generator {
     fn gen(&mut self) -> Result<String, Box<dyn Error>>;
 }
@@ -185,19 +189,27 @@ impl Generator for CounterGen {
     }
 }
 
-pub struct GenOpts {
-    host: String,
-    topic: String,
-    authenticate: bool,
-    username: String,
-    password: String,
-    qos: i32,
-    generator: Box<dyn Generator>,
-    sleep_interval: Duration,
+pub enum GenOpts {
+    Mqtt {
+        host: String,
+        topic: String,
+        authenticate: bool,
+        username: String,
+        password: String,
+        qos: i32,
+        generator: Box<dyn Generator>,
+        sleep_interval: Duration,
+    },
+    Kafka {
+        bootstrap_servers: String,
+        topic: String,
+        generator: Box<dyn Generator>,
+        sleep_interval: Duration,
+    },
 }
 
 impl GenOpts {
-    pub fn new(
+    pub fn new_mqtt(
         host: &str,
         topic: &str,
         username: &str,
@@ -205,35 +217,210 @@ impl GenOpts {
         sleep_interval: Duration,
         generator: Box<dyn Generator>,
     ) -> GenOpts {
-        GenOpts {
+        GenOpts::Mqtt {
             qos: 1,
             host: host.to_string(),
             topic: topic.to_string(),
             authenticate: true,
             username: username.to_string(),
             password: password.to_string(),
-            sleep_interval: sleep_interval,
+            sleep_interval,
+            generator,
+        }
+    }
+
+    pub fn new_kafka(
+        bootstrap_servers: &str,
+        topic: &str,
+        sleep_interval: Duration,
+        generator: Box<dyn Generator>,
+    ) -> GenOpts {
+        GenOpts::Kafka {
+            bootstrap_servers: bootstrap_servers.to_string(),
+            topic: topic.to_string(),
+            sleep_interval,
             generator,
         }
     }
 
     pub fn to_string(&self) -> String {
-        format!("GenOpts: {:} {:} {:}", self.host, self.topic, self.username)
+        match self {
+            GenOpts::Mqtt {
+                host,
+                topic,
+                username,
+                ..
+            } => format!("GenOpts: {:} {:} {:}", host, topic, username),
+            GenOpts::Kafka {
+                bootstrap_servers,
+                topic,
+                ..
+            } => format!("GenOpts: {:} {:}", bootstrap_servers, topic),
+        }
     }
 
-    pub fn to_conn_opts(&self) -> mqtt::connect_options::ConnectOptions {
+    fn sleep_interval(&self) -> Duration {
+        match self {
+            GenOpts::Mqtt { sleep_interval, .. } => sleep_interval.clone(),
+            GenOpts::Kafka { sleep_interval, .. } => sleep_interval.clone(),
+        }
+    }
+}
+
+trait Sink {
+    fn send(&self, data: String) -> Result<(), SinkErr>;
+    fn close(&self) -> Result<(), SinkErr>;
+    fn gen(&mut self) -> Result<String, Box<dyn Error>>;
+}
+
+fn new_sink(opts: GenOpts) -> Result<Box<dyn Sink>, SinkErr> {
+    match opts {
+        GenOpts::Mqtt {
+            host,
+            authenticate,
+            username,
+            password,
+            topic,
+            qos,
+            generator,
+            ..
+        } => Ok(Box::new(MqttSink::new(
+            &host,
+            authenticate,
+            &username,
+            &password,
+            &topic,
+            qos,
+            generator,
+        )?)),
+        GenOpts::Kafka {
+            bootstrap_servers,
+            topic,
+            generator,
+            ..
+        } => Ok(Box::new(KafkaSink::new(
+            &bootstrap_servers,
+            &topic,
+            generator,
+        )?)),
+    }
+}
+
+#[derive(Debug)]
+pub enum SinkErr {
+    Unk(String),
+    NoGenerator(String),
+    ConfigError(String, io::Error),
+    MqttError(mqtt::errors::MqttError),
+    KafkaError(rdkafka::error::KafkaError),
+}
+
+struct MqttSink {
+    client: mqtt::AsyncClient,
+    topic: String,
+    qos: i32,
+    generator: Box<dyn Generator>,
+}
+
+impl MqttSink {
+    fn new(
+        host: &str,
+        authenticate: bool,
+        username: &str,
+        password: &str,
+        topic: &str,
+        qos: i32,
+        generator: Box<dyn Generator>,
+    ) -> Result<MqttSink, SinkErr> {
         let mut opts = mqtt::ConnectOptionsBuilder::new();
 
-        if self.authenticate {
-            opts.user_name(self.username.to_string())
-                .password(self.password.to_string());
+        if authenticate {
+            opts.user_name(username.to_string())
+                .password(password.to_string());
         }
 
-        return opts.finalize();
+        let conn_opts = opts.finalize();
+        let client = mqtt::AsyncClient::new(host.clone())?;
+
+        // Connect and wait for it to complete or fail
+        if let Err(e) = client.connect(conn_opts).wait() {
+            println!("Unable to connect: {:?}", e);
+            return Err(SinkErr::MqttError(e));
+        }
+
+        return Ok(MqttSink {
+            client,
+            qos: qos,
+            topic: topic.to_string(),
+            generator,
+        });
+    }
+}
+
+impl Sink for MqttSink {
+    fn send(&self, data: String) -> Result<(), SinkErr> {
+        // see if we can keep it in the struct, I had lifetime problems trying
+        // to do that
+        let topic = mqtt::Topic::new(&self.client, self.topic.clone(), self.qos);
+
+        topic.publish(data).wait()?;
+        return Ok(());
     }
 
-    pub fn with_generator(&mut self, generator: Box<dyn Generator>) {
-        self.generator = generator;
+    fn close(&self) -> Result<(), SinkErr> {
+        self.client.disconnect(None).wait()?;
+        return Ok(());
+    }
+
+    fn gen(&mut self) -> Result<String, Box<dyn Error>> {
+        self.generator.gen()
+    }
+}
+
+struct KafkaSink {
+    producer: FutureProducer,
+    topic: String,
+    generator: Box<dyn Generator>,
+}
+
+impl KafkaSink {
+    fn new(
+        bootstrap_servers: &str,
+        topic: &str,
+        generator: Box<dyn Generator>,
+    ) -> Result<KafkaSink, SinkErr> {
+        // TODO
+        let msg_timeout_ms = "5000";
+        let producer: FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", bootstrap_servers)
+            .set("produce.offset.report", "true")
+            .set("message.timeout.ms", msg_timeout_ms)
+            .create()?;
+
+        return Ok(KafkaSink {
+            topic: topic.to_string(),
+            producer,
+            generator,
+        });
+    }
+}
+
+impl Sink for KafkaSink {
+    fn send(&self, data: String) -> Result<(), SinkErr> {
+        let record: FutureRecord<String, String> = FutureRecord::to(&self.topic).payload(&data);
+
+        match self.producer.send_result(record) {
+            Ok(_) => Ok(()),
+            Err((err, _)) => Err(SinkErr::KafkaError(err)),
+        }
+    }
+
+    fn close(&self) -> Result<(), SinkErr> {
+        return Ok(());
+    }
+
+    fn gen(&mut self) -> Result<String, Box<dyn Error>> {
+        self.generator.gen()
     }
 }
 
@@ -241,14 +428,20 @@ impl GenOpts {
 pub enum GenError {
     Unk(String),
     NoGenerator(String),
-    ConnInitErr(mqtt::errors::MqttError),
-    ConnErr(mqtt::errors::MqttError),
+    NoSink,
+    InvalidSink(String),
     ConfigError(String, io::Error),
 }
 
-impl From<mqtt::errors::MqttError> for GenError {
-    fn from(error: mqtt::errors::MqttError) -> GenError {
-        GenError::ConnErr(error)
+impl From<mqtt::errors::MqttError> for SinkErr {
+    fn from(error: mqtt::errors::MqttError) -> SinkErr {
+        SinkErr::MqttError(error)
+    }
+}
+
+impl From<rdkafka::error::KafkaError> for SinkErr {
+    fn from(error: rdkafka::error::KafkaError) -> SinkErr {
+        SinkErr::KafkaError(error)
     }
 }
 
@@ -257,6 +450,45 @@ pub fn parse_args() -> Result<GenOpts, GenError> {
         .version("0.1")
         .author("Mariano Guerra <mariano@marianoguerra.org>")
         .about("Generate data and send it to an MQTT broker")
+        .arg(
+            Arg::with_name("interval")
+                .short("i")
+                .value_name("MS")
+                .takes_value(true)
+                .help("Sleep [interval] between messages"),
+        )
+        .arg(
+            Arg::with_name("generator")
+                .short("g")
+                .value_name("GENERATOR_ID")
+                .takes_value(true)
+                .help("Generator type to use"),
+        )
+        .arg(
+            Arg::with_name("generator-config")
+                .short("c")
+                .value_name("PATH")
+                .takes_value(true)
+                .help("Path to config file to setup generator"),
+        )
+        .subcommand(
+            SubCommand::with_name("kafka-gen")
+                .about("Generate data and send it to a Kafka broker")
+                .arg(
+                    Arg::with_name("bootstrap-servers")
+                        .short("b")
+                        .value_name("SERVERS")
+                        .takes_value(true)
+                        .help("Bootstrap server list"),
+                )
+                .arg(
+                    Arg::with_name("topic")
+                        .short("t")
+                        .value_name("TOPIC")
+                        .takes_value(true)
+                        .help("MQTT broker topic"),
+                ),
+        )
         .subcommand(
             SubCommand::with_name("mqtt-gen")
                 .about("Generate data and send it to an MQTT broker")
@@ -287,27 +519,6 @@ pub fn parse_args() -> Result<GenOpts, GenError> {
                         .value_name("PASSWORD")
                         .takes_value(true)
                         .help("MQTT broker authentication password"),
-                )
-                .arg(
-                    Arg::with_name("interval")
-                        .short("i")
-                        .value_name("MS")
-                        .takes_value(true)
-                        .help("Sleep [interval] between messages"),
-                )
-                .arg(
-                    Arg::with_name("generator")
-                        .short("g")
-                        .value_name("GENERATOR_ID")
-                        .takes_value(true)
-                        .help("Generator type to use"),
-                )
-                .arg(
-                    Arg::with_name("generator-config")
-                        .short("c")
-                        .value_name("PATH")
-                        .takes_value(true)
-                        .help("Path to config file to setup generator"),
                 ),
         )
         .get_matches();
@@ -315,28 +526,57 @@ pub fn parse_args() -> Result<GenOpts, GenError> {
     // ------------
 
     // TODO: error handling
-    let submatches = matches.subcommand_matches("mqtt-gen").unwrap();
-    let host = submatches
-        .value_of("host")
-        .unwrap_or("tcp://localhost:1883");
-    let topic = submatches.value_of("topic").unwrap_or("my-topic");
-    let username = submatches.value_of("username").unwrap_or("myusername");
-    let password = submatches.value_of("password").unwrap_or("mypassword");
-    let sleep_interval_ms = value_t!(submatches, "interval", u64).unwrap_or(500);
+    let sleep_interval_ms = value_t!(matches, "interval", u64).unwrap_or(500);
     let sleep_interval = Duration::from_millis(sleep_interval_ms);
-    let gen_type = submatches.value_of("generator").unwrap_or("counter");
-    let gen_config_path = submatches.value_of("generator-config");
+    let gen_type = matches.value_of("generator").unwrap_or("counter");
+    let gen_config_path = matches.value_of("generator-config");
 
-    match generator_from_id(gen_type, gen_config_path) {
-        Ok(generator) => Ok(GenOpts::new(
-            host,
-            topic,
-            username,
-            password,
-            sleep_interval,
-            generator,
-        )),
-        Err(err) => Err(err),
+    match matches.subcommand_name() {
+        Some("mqtt-gen") => {
+            let submatches = matches.subcommand_matches("mqtt-gen").unwrap();
+            let host = submatches
+                .value_of("host")
+                .unwrap_or("tcp://localhost:1883");
+            let topic = submatches.value_of("topic").unwrap_or("my-topic");
+            let username = submatches.value_of("username").unwrap_or("myusername");
+            let password = submatches.value_of("password").unwrap_or("mypassword");
+
+            match generator_from_id(gen_type, gen_config_path) {
+                Ok(generator) => Ok(GenOpts::new_mqtt(
+                    host,
+                    topic,
+                    username,
+                    password,
+                    sleep_interval,
+                    generator,
+                )),
+                Err(err) => Err(err),
+            }
+        }
+        Some("kafka-gen") => {
+            let submatches = matches.subcommand_matches("kafka-gen").unwrap();
+            let bootstrap_servers = submatches
+                .value_of("bootstrap-servers")
+                .unwrap_or("127.0.0.1:9092");
+            let topic = submatches.value_of("topic").unwrap_or("my-topic");
+            match generator_from_id(gen_type, gen_config_path) {
+                Ok(generator) => Ok(GenOpts::new_kafka(
+                    bootstrap_servers,
+                    topic,
+                    sleep_interval,
+                    generator,
+                )),
+                Err(err) => Err(err),
+            }
+        }
+        Some(other) => {
+            eprintln!("Invalid sink: {}", other);
+            Err(GenError::InvalidSink(other.to_string()))
+        }
+        None => {
+            eprintln!("No sink selected");
+            Err(GenError::NoSink)
+        }
     }
 }
 
@@ -354,27 +594,17 @@ fn generator_from_id(id: &str, path: Option<&str>) -> Result<Box<dyn Generator>,
     }
 }
 
-pub fn gen(mut opts: GenOpts) -> Result<(), GenError> {
-    println!("mqtt-gen {:}", opts.to_string());
+pub fn gen(opts: GenOpts) -> Result<(), SinkErr> {
+    println!("gen {:}", opts.to_string());
     println!("Ctrl-c to quit");
 
-    let conn_opts = opts.to_conn_opts();
+    let sleep_interval = opts.sleep_interval();
+    let mut sink = new_sink(opts)?;
 
-    let cli = mqtt::AsyncClient::new(opts.host)?;
-
-    // Connect and wait for it to complete or fail
-    if let Err(e) = cli.connect(conn_opts).wait() {
-        println!("Unable to connect: {:?}", e);
-        return Err(GenError::ConnErr(e));
-    }
-
-    // Create a topic and publish to it
-    let topic = mqtt::Topic::new(&cli, opts.topic, opts.qos);
     loop {
-        match opts.generator.gen() {
+        match sink.gen() {
             Ok(data) => {
-                let tok = topic.publish(data);
-                if let Err(e) = tok.wait() {
+                if let Err(e) = sink.send(data) {
                     println!("Error sending message: {:?}", e);
                     break;
                 }
@@ -384,11 +614,9 @@ pub fn gen(mut opts: GenOpts) -> Result<(), GenError> {
             }
         }
 
-        thread::sleep(opts.sleep_interval);
+        thread::sleep(sleep_interval);
     }
 
-    // Disconnect from the broker
-    let tok = cli.disconnect(None);
-    tok.wait().unwrap();
+    sink.close()?;
     return Ok(());
 }
